1f902f51cc286d42d30794daf745fb98c1671589
João Paiva da Costa Pinto <jo.paiva.cp@gmail.com>
2018-10-13 20:05:25 +0100

    Updated package.json in both folders.

--- Consumer/index.js
+++ Consumer/index.js
@@ -1,22 +1,42 @@
+'use strict';
+
 var mqtt = require('mqtt')
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: 'mysql',
+  user: 'root',
+  password: 'root',
+  database: 'test'
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log('Connected to MySQL database');
+});
+
 var client = mqtt.connect('mqtt://mqtt:1883')
 
 client.on('connect', function () {
-  console.log("Connected")
+  console.log('Connected')
   client.subscribe('presence')
   client.on('message', function (topic, message) {
 
     var messageContent = message.toString()
 
-    console.log("Received " + messageContent)
+    console.log('Received ' + messageContent)
 
-    if (messageContent.split(" ")[1] == 5) disconnect()
+    if (messageContent.split(' ')[1] == 5) disconnect()
 
   })
 })
 
+function connectToDb() {
+
+}
+
 function disconnect() {    
   client.end()  
-  console.log("Disconnected")
+  console.log('Disconnected')
 }
 
--- Producer/index.js
+++ Producer/index.js
@@ -1,16 +1,18 @@
+'use strict';
+
 var mqtt = require('mqtt')
 var client = mqtt.connect('mqtt://mqtt:1883')
 
 client.on('connect', function () {
     
-    console.log("Connected")
+    console.log('Connected')
 
     var i = 1;
     var messageLoop = setInterval(
         () => {
-            var message = "Message " + i.toString()
-            console.log("Sending ", message)
-            client.publish("presence", message)
+            var message = 'Message ' + i.toString()
+            console.log('Sending ', message)
+            client.publish('presence', message)
             i += 1;
             if (i == 6) disconnect(messageLoop)
         },
@@ -21,5 +23,5 @@ client.on('connect', function () {
 function disconnect(messageLoop) {    
     client.end()
     clearInterval(messageLoop)
-    console.log("Disconnected")
+    console.log('Disconnected')
 }
