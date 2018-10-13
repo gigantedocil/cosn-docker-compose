0022a7988584d66b1a32c7cf1fcc5dc4d8886f49
João Paiva da Costa Pinto <jo.paiva.cp@gmail.com>
2018-10-13 19:31:28 +0100

    Updated messaging code.

--- Consumer/index.js
+++ Consumer/index.js
@@ -1,14 +1,22 @@
 var mqtt = require('mqtt')
 var client = mqtt.connect('mqtt://mqtt:1883')
-// var client = mqtt.connect('mqtt://mqtt')
 
 client.on('connect', function () {
   console.log("Connected")
   client.subscribe('presence')
   client.on('message', function (topic, message) {
-    // message is Buffer
-    console.log("Received " + message.toString())
-    // client.end()
+
+    var messageContent = message.toString()
+
+    console.log("Received " + messageContent)
+
+    if (messageContent.split(" ")[1] == 5) disconnect()
+
   })
 })
+
+function disconnect() {    
+  client.end()  
+  console.log("Disconnected")
+}
 
